a8f1cd2a1e38aa6f84073ade6b2687748dfaab40
blueskye16 <fabianuskevin147@gmail.com>
2024-07-06 22:21:22 +0700

    Adding featured article
    - belum bisa diklik
    - ubah responsivenya
    	- tablet 2 artikel
    	- mobile 1 aja => udah

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,7 +12,7 @@
     <title>Dashboard</title>
 </head>
 
-<body class="bg-gray-900">
+<body class="bg-gray-500">
     <nav class="sticky top-0 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700 z-10">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
@@ -115,7 +115,7 @@
                 </div>
             </div>
             <div>
-                <div id="indicators-carousel" class="relative w-full" data-carousel="static">
+                <div id="indicators-carousel" class="relative w-full z-0" data-carousel="static">
                     <!-- Carousel wrapper -->
                     <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
                         <!-- Item 1 -->
@@ -193,23 +193,66 @@
         </div>
     </section>
 
-    <!-- component -->
-    <div class="relative flex max-w-[24rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md mt-10">
-        <div class="relative m-0 overflow-hidden rounded-none bg-transparent bg-clip-border text-gray-700 shadow-none">
-            <img src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1471&amp;q=80"
-                alt="ui/ux review check" />
-        </div>
-        <div class="p-6">
-            <h4
-                class="block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-                UI/UX Review Check
-            </h4>
-            <p class="mt-3 block font-sans text-xl font-normal leading-relaxed text-gray-700 antialiased">
-                Because it's about motivating the doers. Because I'm here to follow my
-                dreams and inspire others.
-            </p>
-        </div>
-    </div>
+    <main>
+
+        {{-- Featured Article --}}
+        <section class="max-w-full">
+            <h1 class="text-xl text-center mt-4">Konten Unggulan</h1>
+            <div class="h-[80vh] grid grid-cols-1 gap-4 lg:grid-cols-3 mt-4 mx-6">
+                <div
+                    class="relative flex max-w-[24rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md mx-auto lg:mx-0">
+                    <div
+                        class="relative m-0 overflow-hidden rounded-xl bg-transparent bg-clip-border text-gray-700 shadow-none">
+                        <img src="/images/bg-hero.jpg" alt="featured image" class="antialiased" />
+                    </div>
+                    <div class="p-6">
+                        <h4
+                            class="block font-sans text-sm/5 font-normal tracking-normal text-blue-gray-900 antialiased">
+                            Sabtu, 6 Juli 2024
+                        </h4>
+                        <p class="mt-1 block font-sans text-xl font-normal leading-relaxed text-gray-700 antialiased">
+                            Because it's about motivating the doers. Because I'm here to follow my dreams and inspire
+                            others.
+                        </p>
+                    </div>
+                </div>
+                <div
+                    class="relative flex max-w-[24rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md mx-auto lg:mx-0">
+                    <div
+                        class="relative m-0 overflow-hidden rounded-xl bg-transparent bg-clip-border text-gray-700 shadow-none">
+                        <img src="/images/bg-hero.jpg" alt="featured image" />
+                    </div>
+                    <div class="p-6">
+                        <h4
+                            class="block font-sans text-sm/5 font-normal tracking-normal text-blue-gray-900 antialiased">
+                            Sabtu, 6 Juli 2024
+                        </h4>
+                        <p class="mt-1 block font-sans text-xl font-normal leading-relaxed text-gray-700 antialiased">
+                            Because it's about motivating the doers. Because I'm here to follow my dreams and inspire
+                            others.
+                        </p>
+                    </div>
+                </div>
+                <div
+                    class="relative flex max-w-[24rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md mx-auto lg:mx-0">
+                    <div
+                        class="relative m-0 overflow-hidden rounded-xl bg-transparent bg-clip-border text-gray-700 shadow-none">
+                        <img src="/images/bg-hero.jpg" alt="featured image" />
+                    </div>
+                    <div class="p-6">
+                        <h4
+                            class="block font-sans text-sm/5 font-normal tracking-normal text-blue-gray-900 antialiased">
+                            Sabtu, 6 Juli 2024
+                        </h4>
+                        <p class="mt-1 block font-sans text-xl font-normal leading-relaxed text-gray-700 antialiased">
+                            Because it's about motivating the doers. Because I'm here to follow my dreams and inspire
+                            others.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 
     <h1 class="text-2xl">helo</h1>
     <h1 class="text-2xl">helo</h1>
