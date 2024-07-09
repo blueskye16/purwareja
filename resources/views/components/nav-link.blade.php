8c81e59fcd350fd159782342f7fd9f93a03da3cf
blueskye16 <fabianuskevin147@gmail.com>
2024-07-09 19:26:11 +0700

    NavBar, changing active route interface

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,3 @@
+<a {{ $attributes }}
+    class="block py-2 px-3 {{ $active ? 'text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent' : 'text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' }}"
+    aria-current="{{ $active ? 'page' : false }}">{{ $slot }}</a>
